16da4995b39a78c43f0bd5962e0fd961d750e4f2
atrkv <144206334+atrkv@users.noreply.github.com>
2023-10-11 10:21:08 +0300

    Add files via upload

---
+++ homework/hwtask1/hwtask1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void){
+	
+	printf("var1\n");
+	int var1;
+	scanf("%d",&var1);
+	//printf("%d\n",var1);
+
+	printf("\nvar2\n");
+	int var2;
+	scanf("%d",&var2);
+	//	printf("%d\n",var2);
+
+	printf("result: %d\n", var1*var2);
+
+return 0;
+}
---
+++ homework/hwtask1/hwtask1_2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void){
+	
+	printf("var1\n");
+	long int var1;
+	scanf("%ld",&var1);
+	//printf("%d\n",var1);
+
+	printf("\nvar2\n");
+	long int var2;
+	scanf("%ld",&var2);
+	//	printf("%d\n",var2);
+
+	printf("result: %ld\n", var1*var2);
+
+return 0;
+}
